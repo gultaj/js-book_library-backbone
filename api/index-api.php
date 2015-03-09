4c3b3fa05afbae04ed637ca531932f287f8e3296
gultaj <yaschick.mail@gmail.com>
2015-03-09 12:17:28 +0300

    rename file

--- api/index-api.php
+++ api/index-api.php
@@ -1,18 +1,6 @@
 <?php 
 require 'vendor/autoload.php';
 
-function my_log_file( $msg, $name = '' )
-{
-    // Print the name of the calling function if $name is left empty
-    $trace=debug_backtrace();
-    $name = ( '' == $name ) ? $trace[1]['function'] : $name;
-
-    $error_dir = 'D:/php_error.log';
-    $msg = print_r( $msg, true );
-    $log = $name . "  |  " . $msg . "\n";
-    error_log( $log, 3, $error_dir );
-}
-
 $app = new \Slim\Slim();
 
 $dsn = "mysql:host=localhost;dbname=booklibrary";
